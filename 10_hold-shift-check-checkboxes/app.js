d705194d89d710558260f25860df5be5aa0de908
yumcoding <64473833+yumcoding@users.noreply.github.com>
2021-04-20 12:22:55 +0900

    Update app.js

--- 10_hold-shift-check-checkboxes/app.js
+++ 10_hold-shift-check-checkboxes/app.js
@@ -2,29 +2,28 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-function handleCheck(e) {
-  // Check if they had the shift key down
-  // AND check that they are checking it
+const handleCheck = e => {
+  
   let inBetween = false;
-  if (e.shiftKey && this.checked) {
-    // go ahead and do what we please
-    // loop over every single checkbox
-    checkboxes.forEach((checkbox) => {
-      console.log(checkbox);
-      if (checkbox === this || checkbox === lastChecked) {
+
+  if(e.shiftKey && e.target.checked){
+
+    checkboxes.forEach((checkbox)=>{
+
+      if(checkbox === e.target || checkbox === lastChecked){
         inBetween = !inBetween;
-        console.log("Starting to check them in between!");
       }
 
-      if (inBetween) {
-        checkbox.checked = true;
+      if(inBetween){
+        checkbox.checked=true;
       }
+
     });
   }
 
-  lastChecked = this;
+  lastChecked = e.target;
 }
 
-checkboxes.forEach((checkbox) =>
-  checkbox.addEventListener("click", handleCheck)
-);
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
